6ec81b17fe405e836d95772c536670db4a016d4a
William O'Toole <otoolew@gmail.com>
2016-05-11 14:22:07 -0400

    Set up of base classes

---
+++ WWLinePainting/src/WWLinePainting/Models/Vehicle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WWLinePainting.Models
+{
+    public class Vehicle
+    {
+        public string VIN { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Year { get; set; }
+        public string Type { get; set; }
+        public string License { get; set; }
+    }
+}
