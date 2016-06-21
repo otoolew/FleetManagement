221e55a36ad3782ef542ffc2946ca4966c84fb8a
William O'Toole <otoolew@gmail.com>
2016-06-21 13:04:08 -0400

    Changes to Workspace

--- BusinessFlow/src/BusinessFlow/Models/Vehicle.cs
+++ BusinessFlow/src/BusinessFlow/Models/Vehicle.cs
@@ -16,66 +16,66 @@ namespace BusinessFlow.Models
         [DataType(DataType.Text)]
         [Display(Name = "VIN")]
         [StringLength(100, MinimumLength = 5, ErrorMessage = "VIN must be between 5 and 100 characters long.")]
-        [Column(TypeName ="varchar")]
+        [Column(TypeName ="varchar(50)")]
         public string VIN { get; set; }
 
         [Required]
         [DataType(DataType.Text)]
         [Display(Name = "Name")]
         //[StringLength(100, MinimumLength = 5, ErrorMessage = "Name must be between 5 and 100 characters long.")]
-        [Column(TypeName = "varchar")]
+        [Column(TypeName = "varchar(50)")]
         public string Name { get; set; }
 
         [Required]
         [DataType(DataType.Text)]
         [Display(Name = "Make")]
         //[StringLength(50, MinimumLength = 3, ErrorMessage = "Make must be between 3 and 50 characters long.")]
-        [Column(TypeName = "varchar")]
+        [Column(TypeName = "varchar(50)")]
         public string Make { get; set; }
 
         [Required]
         [DataType(DataType.Text)]
         [Display(Name = "Model")]
         //[StringLength(50, MinimumLength = 3, ErrorMessage = "Model must be between 3 and 50 characters long.")]
-        [Column(TypeName = "varchar")]
+        [Column(TypeName = "varchar(50)")]
         public string Model { get; set; }
 
         [Required]
         [DataType(DataType.Text)]
         [Range(1940,2100, ErrorMessage = "Please enter a valid Year")]
         [Display(Name = "Year")]
-        [Column(TypeName = "varchar")]
+        [Column(TypeName = "varchar(50)")]
         public string Year { get; set; }
 
         [Required]
         [DataType(DataType.Text)]
         [Display(Name = "Type")]
         //[StringLength(50, MinimumLength = 3, ErrorMessage = "Type must be between 3 and 50 characters long.")]
-        [Column(TypeName = "varchar")]
+        [Column(TypeName = "varchar(50)")]
         public string Type { get; set; }
 
         [DataType(DataType.Text)]
         [Display(Name = "License")]
         //[StringLength(50, MinimumLength = 10, ErrorMessage = "License must be between 10 and 50 characters long.")]
-        [Column(TypeName = "varchar")]
+        [Column(TypeName = "varchar(50)")]
         public string License { get; set; }
 
         [DataType(DataType.Text)]
         [Display(Name = "Registration")]
         //[StringLength(50, MinimumLength = 10, ErrorMessage = "License must be between 10 and 50 characters long.")]
-        [Column(TypeName = "varchar")]
+        [Column(TypeName = "varchar(50)")]
         public string Registration { get; set; }
 
         [DataType(DataType.Text)]
         [Display(Name = "Engine")]
         //[StringLength(50, MinimumLength = 10, ErrorMessage = "License must be between 10 and 50 characters long.")
-        [Column(TypeName = "varchar")]
+        [Column(TypeName = "varchar(50)")]
         public string Engine { get; set; }
 
         [DataType(DataType.Text)]
         [Display(Name = "Transmission")]
         //[StringLength(50, MinimumLength = 10, ErrorMessage = "License must be between 10 and 50 characters long.")]
-        [Column(TypeName = "varchar")]
+        [Column(TypeName = "varchar(50)")]
         public string Transmission { get; set; }
 
         public virtual ICollection<Part> Parts { get; set; }
